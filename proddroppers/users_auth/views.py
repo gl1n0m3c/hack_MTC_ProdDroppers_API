202659e1ca0bfdf194d2d732be56876d39e7fc94
AleksandrSafarov <aleks.safarov2005@gmail.com>
2023-11-19 21:18:49 +0300

    wrong import fixed

--- proddroppers/users_auth/views.py
+++ proddroppers/users_auth/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users_auth.serializers import RegisterSerializer, UserSerializer
+from users_auth.serializers import RegisterSerializer
 
 
 class RegisterUserAPIView(generics.CreateAPIView):
