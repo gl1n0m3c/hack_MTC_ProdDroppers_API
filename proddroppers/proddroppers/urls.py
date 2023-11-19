9f9df5200b23b01ad24acd581ffdf8daddba18f0
Nicolashka <nperewerzew2005@yandex.ru>
2023-11-19 19:50:06 +0300

    after alex...

--- proddroppers/proddroppers/urls.py
+++ proddroppers/proddroppers/urls.py
@@ -2,12 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("users_auth.urls")),
-    path('api-token-auth', views.obtain_auth_token)
 ]
 
 
---
+++ proddroppers/users/serializers.py
@@ -0,0 +1,8 @@
+from proddroppers.users_auth import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "username"]
