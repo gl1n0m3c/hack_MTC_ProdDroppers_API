bb749af12b41ca77cd89928649f5464494868727
AleksandrSafarov <aleks.safarov2005@gmail.com>
2023-11-20 23:11:47 +0300

    added change password

--- proddroppers/users_auth/urls.py
+++ proddroppers/users_auth/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from users_auth.views import LoginUserAPIView, RegisterUserAPIView
+from users_auth.views import (
+    ChangePasswordView,
+    LoginUserAPIView,
+    ChangePasswordView,
+    RegisterUserAPIView,
+)
 
 urlpatterns = [
     path("register", RegisterUserAPIView.as_view()),
     path("login", LoginUserAPIView.as_view()),
+    path("changepassword", ChangePasswordView.as_view()),
 ]
--- proddroppers/users_auth/views.py
+++ proddroppers/users_auth/views.py
@@ -1,8 +1,19 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from . import serializers
+from rest_framework.permissions import IsAuthenticated
 
-from users_auth.serializers import LoginSerializer, RegisterSerializer
+from users_auth.serializers import (
+    LoginSerializer,
+    RegisterSerializer,
+    ChangePasswordSerializer,
+)
 
 
 class RegisterUserAPIView(APIView):
@@ -31,6 +42,21 @@ class LoginUserAPIView(APIView):
                     "success": ["True"],
                     "id": [user.id],
                     "description": ["Авторизация прошла успешно"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "success": ["True"],
+                    "description": ["Пароль успешно изменен"],
                 },
                 status=status.HTTP_200_OK,
             )
