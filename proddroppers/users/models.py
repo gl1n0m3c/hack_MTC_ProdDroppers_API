ba257d86b2d4b4d6bb025d645ee8cfaf4ee55d34
Nicolashka <nperewerzew2005@yandex.ru>
2023-11-20 23:09:45 +0300

    flake)

--- proddroppers/users/models.py
+++ proddroppers/users/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-
 from sorl.thumbnail import delete
 
 from core.models import ImageOperations
