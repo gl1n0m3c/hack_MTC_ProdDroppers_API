9989c28f52d3a2d33e897c00295a9e93fc99d2dd
AleksandrSafarov <aleks.safarov2005@gmail.com>
2023-11-21 18:12:18 +0300

    added change email, first name and last

--- proddroppers/users_auth/views.py
+++ proddroppers/users_auth/views.py
@@ -89,3 +89,70 @@ class ChangePasswordView(APIView):
             },
             status=status.HTTP_200_OK,
         )
+
+
+class ChangeEmailAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            user = User.objects.get(id=request.data["id"])
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "success": ["False"],
+                    "description": ["Такого пользователя нету"],
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user.username == request.data["new_email"]:
+            return Response(
+                {
+                    "success": ["False"],
+                    "description": ["Новая почта совпадает со старой"],
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            User.objects.get(username=request.data["new_email"])
+        except User.DoesNotExist:
+            user.username = request.data["new_email"]
+            user.email = request.data["new_email"]
+            user.save()
+            return Response(
+                {
+                    "success": ["True"],
+                    "description": ["Почта успешно изменена"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {
+                "success": ["False"],
+                "description": ["Такая почта уже зарегистрирована"],
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class ChangeFirstNameAndLastNameAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            user = User.objects.get(id=request.data["id"])
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "success": ["False"],
+                    "description": ["Такого пользователя нету"],
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user.last_name = request.data["last_name"]
+        user.first_name = request.data["first_name"]
+        user.save()
+        return Response(
+            {
+                "success": ["True"],
+                "description": ["Данные успешно изменены"],
+            },
+            status=status.HTTP_200_OK,
+        )
