bd31f87ffe611786d78c63aed90056d03a2241a2
AleksandrSafarov <aleks.safarov2005@gmail.com>
2023-11-23 19:11:27 +0300

    .

--- proddroppers/rooms/consumers.py
+++ proddroppers/rooms/consumers.py
@@ -11,7 +11,6 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
@@ -32,7 +31,7 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             {
                 "type": "chatroom_message",
-                "message": "GDSAHD",
+                "message": message,
                 "username": username,
             },
         )
--- proddroppers/rooms/views.py
+++ proddroppers/rooms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
 def chatPage(request, room_name):
